12622863b980e0bae8cf16d3050808ac81e584cf
Randy Aguero Bermudez <randyaguero1009@gmail.com>
2021-06-08 20:46:19 -0600

    Prueba

--- Gestor.java
+++ Gestor.java
@@ -17,6 +17,7 @@ public class Gestor
     {
         x = 0;
         System.out.println("PRueba Merge");
+        System.out.println("Prueb");
         System.out.println("Prueba mezcla de archivos");
         System.out.println("Sin conflictos");
         System.out.println("PRueba adicion de codigo");
